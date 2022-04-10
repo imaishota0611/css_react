81ccaecc416c440314310ff0d0b44b4080f2755a
imaishota0611 <102264809+imaishota0611@users.noreply.github.com>
2022-04-11 08:37:15 +0900

    Initial commit

---
+++ src/components/Emotion.jsx
@@ -0,0 +1,31 @@
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { jsx, css } from "@emotion/react";
+
+export const Emotion = () => {
+  const containerStyle = css`
+    border: solid 2px #392eff;
+    border-radius: 20px;
+    padding: 8px;
+    margin: 8px;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+  `;
+  const titleStyle = css({
+    margin: 0,
+    color: "#3d84a8"
+  });
+  const buttonStyle = css({
+    backgroundColor: "#abedd8",
+    border: "none",
+    padding: "8px",
+    borderRadius: "8px"
+  });
+  return (
+    <div css={containerStyle}>
+      <p css={titleStyle}>- Emotion -</p>
+      <button css={buttonStyle}>FIGHT!</button>
+    </div>
+  );
+};
